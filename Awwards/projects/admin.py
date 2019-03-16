ecc1fad2d66d333db8eadcf93b5ef057d20ab3b6
abby-shabi <abbyshabi@gmail.com>
2019-03-16 16:08:33 +0300

    registered admin

--- Awwards/projects/admin.py
+++ Awwards/projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Project,Profile,Comments,DesignRating,UsabilityRating,ContentRating
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Comments)
+admin.site.register(DesignRating)
+admin.site.register(UsabilityRating)
+admin.site.register(ContentRating)
