c6ca899dd208bdbd311587291f917bb08060282a
abby-shabi <abbyshabi@gmail.com>
2019-03-16 15:59:27 +0300

    imported files into the views.py file

--- Awwards/projects/views.py
+++ Awwards/projects/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProjectForm,ReviewForm,CommentForm
+from .models import Project,Profile,Comments,DesignRating,UsabilityRating,ContentRating,Review
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer,ProjectSerializer
 
-# Create your views here.
+
